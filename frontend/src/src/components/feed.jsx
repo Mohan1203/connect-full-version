754929291a18f4f8e468bd1c462a4e81fb95e79f
unknown <mrahir101@gmail.com>
2023-03-22 12:05:30 +0530

    Create feed and likes feature updated

--- frontend/src/src/components/feed.jsx
+++ frontend/src/src/components/feed.jsx
@@ -1,63 +1,86 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../style/feed.css";
 import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 import { GoComment } from "react-icons/go";
 import { BiShare } from "react-icons/bi";
 import profilepic from "../media/profilePic.webp";
 import { Link } from "react-router-dom";
+import axios from "axios";
+
 
 function Feed() {
-    const [likebtn, setlikebtn] = useState(false);
-    const [likes, setlikes] = useState(0);
-    const userID = 123;
 
+    const [data, setData] = useState([]);
 
+   useEffect(()=>{
+     axios({
+           method:"GET",
+           url:"http://localhost:3333",
+           headers:{
+               "Content-Type":"application/json",
+               "Authorization":localStorage.getItem("token")
+            }
+        }).then((res)=>{
+            console.log(res.data)
+            setData(res.data)
+        }).catch(err=>console.log(err))
+    },[])
 
-
-    const isLikes = (e) => {
-        e.preventDefault();
-        setlikebtn(!likebtn);
-        if (likebtn) {
-            setlikes(likes - 1);
-        } else {
-            setlikes(likes + 1);
-        }
+    function likeimage(id){
+        
+        console.log(id)
+        axios({
+            method:"PUT",
+            url:`http://localhost:3333/like/${id}`,
+            headers:{
+                "Content_Type":"application/json",
+                "Authorization":localStorage.getItem("token")
+            }
+        }).then((res)=>{
+            console.log(res.data)
+        }).catch((err)=>{
+            console.log(err)
+        })
     }
 
-    const heart = likebtn ? <AiFillHeart size={30} color={"red"} /> : <AiOutlineHeart size={30} />
-
     return (
-        <div className="feed-container">
+        <div className="feed-container" >
             <div className="feed-posts">
-                <div className="feed-post">
-                    <div className="post-header">
-                        <Link to={`/profile/${userID}`}>
-                            <img src={profilepic} className="user-profilepic" />
-                            <h5>username</h5></Link>
-                    </div>
-                    <div className="full-post">
-                        <div className="post-body">
-                            <img src={profilepic} alt="profile pic" />
-                        </div>
-                        <div className="post-icons">
-                            <form>
-                                <button onClick={isLikes}>{heart}</button>
-                                <Link to={"/comments"}><button ><GoComment size={29} /></button></Link>
-                                <button ><BiShare size={29} /></button>
-                            </form>
-                        </div>
-                    </div>
+                {data.map((item,index) => {
+                    return (
+                        <div className="feed-post" key={index}>
+                            <div className="post-header">
+                                <Link to={`/profile/${item.userID._id}`}>
+                                    <img src={`http://localhost:3333/profilephoto/${item.userID.profilephoto}`} className="user-profilepic" alt="Dp"/>
+                                    <h5>{item.userID.username}</h5></Link>
+                            </div>
+                                <div className="full-post">
+                                <div className="post-body">
+                                    <img src={`http://localhost:3333/posts/${item.imageName}`} alt="profile pic" />
+                                </div>
+                                <div className="post-icons">
+                                    <form>
+                                        <button onClick={(e)=>{
+                                            e.preventDefault();
+                                            likeimage(item._id)
+                                        }}>{<AiFillHeart size={30} color={"red"} />}</button>
+                                        <Link to={"/comments"}><button ><GoComment size={29} /></button></Link>
+                                        <button ><BiShare size={29} /></button>
+                                    </form>
+                                </div>
+                            </div>
 
-                    <div className="post-footer">
-                        <h5>{likes}</h5>
-                        <div className="post-description">
-                            <a href="#" >username</a>
-                            <p>making it look like readable English. Many desktop publishing packages and web page editors .</p>
+                            <div className="post-footer">
+                                <h5>{item.likes}</h5>
+                                <div className="post-description">
+                                    <Link to={`/profile/${item.userID._id}`} >{item.userID.username}</Link>
+                                    <p>{item.caption}</p>
+                                </div>
+
+                            </div> 
                         </div>
-
-                    </div>
-                </div>
-
+                    )
+                })}
 
             </div>
         </div>
